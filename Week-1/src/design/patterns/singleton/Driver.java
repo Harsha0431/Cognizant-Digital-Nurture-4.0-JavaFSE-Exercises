7a6cdc53272c9b2c016c45078e77709c9c4550e9
Harsha0431 <harshavardhan0431@gmail.com>
2025-06-17 23:40:23 +0530

    Week-1: In Design patterns singleton pattern done

---
+++ Week-1/src/design/patterns/singleton/Driver.java
@@ -0,0 +1,41 @@
+package design.patterns.singleton;
+
+
+import java.util.Date;
+
+/*
+Singleton pattern:
+Category: Creational Design Pattern
+
+Definition:
+Singleton pattern restricts the instantiation of a class and ensures that only one instance of the class
+exists in the Java Virtual Machine.
+
+- Singleton pattern is used for logging, drivers objects, caching, and thread pool.
+
+To implement a singleton pattern, we have different approaches, but all of them have the following common concepts.
+
+- Private constructor to restrict instantiation of the class from other classes.
+- Private static variable of the same class that is the only instance of the class.
+- Public static method that returns the instance of the class,
+this is the global access point for the outer world to get the instance of the singleton class.
+*/
+public class Driver {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger();
+        System.out.println("Called from Driver class: " + logger.sayHello());
+        logger.setLoggerLoginDate(new Date());
+        System.out.println("Driver class logger loginDate: " + logger.getLoggerLoginDate());
+
+        // Run test class `runTest` method to check if Logger is using same instance or not
+        Test.runTest();
+    }
+}
+
+
+class Test{
+    public static void runTest(){
+        Logger logger = Logger.getLogger();
+        System.out.println("Test class logger loginDate: " + logger.getLoggerLoginDate());
+    }
+}
---
+++ Week-1/src/design/patterns/singleton/Logger.java
@@ -0,0 +1,32 @@
+package design.patterns.singleton;
+
+
+import java.util.Date;
+
+// This is singleton class which doesn't allow any other class to create an instance of it.
+// Rather encourages other classes to share the same instance.
+public class Logger {
+    private static Logger logger; // If we initialize here itself then it is called eager initialization.
+    private Date loginDate;
+
+    // As the logger class is defined as private, JVM doesn't allow any other classes to create a instance of this class.
+    private Logger(){}
+
+    public static Logger getLogger(){
+        if(logger == null)
+            logger = new Logger(); // It is lazy initialization.
+        return logger;
+    }
+
+    public String sayHello(){
+        return "Hello, from Logger skeleton class";
+    }
+
+    public void setLoggerLoginDate(Date date){
+        loginDate = date;
+    }
+
+    public Date getLoggerLoginDate(){
+        return loginDate;
+    }
+}
