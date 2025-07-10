15aef7815c1272cab4c566aebde74bdd477e45e1
Harsha0431 <harshavardhan0431@gmail.com>
2025-07-10 14:54:23 +0530

    Week-4: Spring Rest all mandatory hands on done and results pdf updated

--- Week-4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Week-4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,20 +1,40 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/country")
 public class CountryController {
+    @Autowired
+    private CountryService countryService;
+    private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
 
     @GetMapping
     public Country getCountryIndia(){
         ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
         return context.getBean("country", Country.class);
+    }
+
+    @GetMapping("{code}")
+    public Country getCountry(@PathVariable String code){
+        try{
+            return countryService.getCountry(code);
+        }
+        catch (Exception e){
+            logger.error(e.toString());
+            return null;
+        }
     }
 }
